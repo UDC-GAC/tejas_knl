54882cdef44466fc0a5df9be599ba3b01f282b46
Marcos Horro <marcos.horro@udc.es>
2019-01-03 15:54:27 +0100

    Still working on core affinity

--- Tejas-Simulator/Tejas/src/simulator/config/SystemConfig.java
+++ Tejas-Simulator/Tejas/src/simulator/config/SystemConfig.java
@@ -124,8 +124,30 @@ public class SystemConfig
     									 4,24,48,6,26,-1,
     									 10,32,-1,12,34,54,
     									 16,40,58,18,42,60};
-    public static String addrFilePath = "/home/marcos.horro/tejas_installation_kit/Tejas-Simulator/Tejas/addr.txt";
-    public static String mapFilePath = "/home/marcos.horro/mapping.knl";
+    
+    
+    /*
+    public static int[] coreAffinity = {0,1,20,21,22,23,52,53,
+                                        28,29,50,51,2,3,30,31,
+                                        36,37,56,57,8,9,38,39,
+                                        44,45,62,63,14,15,46,47,
+                                        4,5,24,25,48,49,6,7,26,27,
+                                        10,11,32,33,12,13,34,35,54,55,
+                                        16,17,40,41,58,59,18,19,42,43,60,61};
+    */
+    
+    public static int[] coreAffinity = {0,1,2,3,4,5,6,7,8,9,
+            10,11,12,13,14,15,16,17,18,19,
+            20,21,22,23,24,25,26,27,28,29,
+            30,31,32,33,34,35,36,37,38,39,
+            40,41,42,43,44,45,46,47,48,49,
+            50,51,52,53,54,55,56,57,58,59,
+            60,61,62,63
+    };
+    
+    
+    public static String addrFilePath = "/home/mhorro/tejas-git/Tejas-Simulator/Tejas/addr.txt";
+    public static String mapFilePath = "/home/mhorro/mapping.knl";
     public static byte[] mappingKNL;	
 
     //FIXME
