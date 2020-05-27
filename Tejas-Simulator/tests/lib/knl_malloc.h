731384261e49e540c874b668e9d6bfe7a75da89f
Marcos Horro <marcos.horro@udc.es>
2020-05-27 18:24:38 +0200

    Cleaning some useless code...

--- Tejas-Simulator/tests/lib/knl_malloc.h
+++
@@ -1,9 +0,0 @@
-namespace KNL
-{
-  class KNLib
-  {
-  public:
-    static void* mc_malloc(long bytes) { return malloc(bytes); };
-    static void* ddr_malloc(long bytes) { return malloc(bytes); };
-  };
-}
